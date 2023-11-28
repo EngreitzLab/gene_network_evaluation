1ddbd133d4be85e84bac74d91cc19d1416152a89
Revant Gupta <25486108+aron0093@users.noreply.github.com>
2023-11-28 11:11:34 -0800

    Update template.py
    Template for adding evaluation measures. Cell x program scores are expected in the anndata format with metadata stored according to specification (see anndata documentation)

--- evals/template.py
+++ evals/template.py
@@ -1,1 +1,21 @@
+import os
+import argparse
 
+def main(adata, optional=None, inplace=True):
+  
+  adata = adata.copy() if inplace else adata
+  
+  # Update adata with eval measure
+  adata.var['eval_measure'] = None
+
+  if inplace: return adata.var['eval_measure']
+	
+if __name__=='__main__':
+  parser = argparse.ArgumentParser()
+
+	parser.add_argument('anndataObj')
+	parser.add_argument('-o', '--optional') 
+
+	args = parser.parse_args()
+    
+	main(args.anndataObj, optional=args.optional)
