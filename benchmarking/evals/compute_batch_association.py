c4877a79a92468d03416ae8ecfd11dd64af291b9
Revant Gupta <regu@sh03-ln06.stanford.edu>
2023-12-04 16:15:09 -0800

    Added posthoc tests to compute batch-factor score.

--- benchmarking/evals/compute_batch_association.py
+++ benchmarking/evals/compute_batch_association.py
@@ -1,7 +1,12 @@
 import os
 import argparse
 
+import numpy as np
+import pandas as pd
+
 from scipy import stats, sparse
+from scikit_posthocs import posthoc_dscf
+
 from joblib import Parallel, delayed
 from tqdm.auto import tqdm
 
@@ -22,6 +27,23 @@ def perform_kruskall_wallis(mdata, prog_key='prog',
     mdata[prog_key].var.loc[prog_nam, 'kruskall_wallis_stat'] = stat
     mdata[prog_key].var.loc[prog_nam, 'kruskall_wallis_pval'] = pval
 
+# Perfom connover's posthoc test
+def perform_conover_iman(mdata, prog_key='prog', 
+                         prog_nam=None, batch_key='batch'):
+    
+    prog_df = pd.DataFrame(mdata[prog_key][:, prog_nam].X, columns=[prog_nam])
+    prog_df[batch_key] = mdata[prog_key].obs[batch_key].astype(str).values
+
+    # Create combined statistic with p-vals for each batch
+    p_vals = posthoc_dscf(prog_df, val_col=prog_nam, group_col=batch_key)
+    for i, batch in enumerate(mdata[prog_key].obs[batch_key].astype(str).unique()):
+        comb_stat, comb_pval = stats.combine_pvalues(p_vals.loc[p_vals.index!=batch,
+                                                                batch])
+        
+        prog_idx = mdata[prog_key].var.index.get_loc(prog_nam)
+        mdata[prog_key].varm['batch_association_stat'][prog_idx,i] = comb_stat
+        mdata[prog_key].varm['batch_association_pval'][prog_idx,i] = comb_pval
+
 # TODO: Add one way ANOVA, MANOVA, multi-variate KW
 def compute_batch_association(mdata, batch_key='batch', n_jobs=1,
 	                          prog_key='prog', inplace=True):
@@ -39,9 +61,23 @@ def compute_batch_association(mdata, batch_key='batch', n_jobs=1,
                                                              batch_key=batch_key) \
                                                              for prog_nam in tqdm(mdata[prog_key].var_names,
                                                              desc='Testing batch association', unit='programs'))
-
-    if not inplace: return mdata[prog_key].var.loc[:, ['kruskall_wallis_stat', 
-                                                       'kruskall_wallis_pval']]
+    
+    mdata[prog_key].varm['batch_association_stat'] = np.zeros((mdata[prog_key].shape[1],
+                                                               mdata[prog_key].obs[batch_key].unique().shape[0]))
+    mdata[prog_key].varm['batch_association_pval'] = np.ones((mdata[prog_key].shape[1],
+                                                              mdata[prog_key].obs[batch_key].unique().shape[0]))
+    
+    Parallel(n_jobs=n_jobs, backend="threading")(delayed(perform_conover_iman)(mdata, 
+                                                                               prog_key=prog_key,
+                                                                               prog_nam=prog_nam, 
+                                                                               batch_key=batch_key) \
+                                                             for prog_nam in tqdm(mdata[prog_key].var_names,
+                                                             desc='Identifying differential batches', unit='programs'))
+    
+    if not inplace: return (mdata[prog_key].var.loc[:, ['kruskall_wallis_stat', 
+                                                       'kruskall_wallis_pval']],
+                           mdata[prog_key].varm['batch_association_stat'], 
+                           mdata[prog_key].varm['batch_association_pval'])
 	
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
