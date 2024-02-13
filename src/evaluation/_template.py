1d63bfc430bc747bab83b4bfebdefc6f398b24d8
Revant Gupta <regu@sh03-04n21.int>
2024-02-12 17:21:18 -0800

    Updated template for memory efficient copy mode computation and added docstring.

--- src/evaluation/_template.py
+++ src/evaluation/_template.py
@@ -1,6 +1,8 @@
 import os
 import argparse
 
+import mudata
+
 # The template is intended to be a general guide on how to organise your code
 # so feel free to write functions as required.
 # Extended the parser and function with specific parameters required for
@@ -12,21 +14,51 @@ import argparse
 def helper_function(mdata, intermediate=None):
     return
 
-def compute_eval_measure_(some_values):
+# Rename function to compute_{eval_measure}_
+def compute_eval_measure_(some_values, somearg=None):
     return eval_measure
 
 # Rename function to compute_{eval_measure}
-def compute_eval_measure(mdata, n_jobs=-1, prog_key='prog', 
-                         rna_key='rna', atac_key='atac', inplace=True):
+def compute_eval_measure(mdata, prog_key='prog', data_key='rna', 
+                         somearg=None,  n_jobs=1, inplace=True, 
+                         **kwargs):
+    """
+    Computes eval_measure.
+
+    ARGS
+        mdata : MuData
+            mudata object containing anndata of program scores and cell-level metadata.
+        prog_key: 
+            index for the gene program anndata object (mdata[prog_key]) in the mudata object.
+        data_key: str
+            index of the genomic data anndata object (mdata[data_key]) in the mudata object.
+        somearg: str (optional)
+            description of somearg.
+        n_jobs: int (default: 1)
+            number of threads to run processes on.
+        inplace: Bool (default: True)
+            update the mudata object inplace or return a copy
     
-    #TODO: Don't copy entire mudata only relevant Dataframe
-    mdata = mdata.copy() if not inplace else mdata
+    CREATES
+        mdata[prog_key].var['eval_measure']
+    
+    RETURNS 
+        if not inplace:
+            mdata[prog_key].var['eval_measure']
+            
+    """
+    
+    # Only copy the anndatas that will be used in the eval
+    if not inplace:
+        mdata = mudata.MuData({prog_key: mdata[prog_key].copy(),
+                               data_key: mdata[data_key].copy()})
   
     intermediate_output = helper_function(mdata, 
                                           intermediate=intermediate_input)
 
     # Compute & update mudata with eval measure
-    mdata[prog_key].var['eval_measure'] = compute_eval_measure_(intermediate_output)
+    mdata[prog_key].var['eval_measure'] = compute_eval_measure_(intermediate_output, 
+                                                                somearg=somearg)
 
     # Not all eval measures might be condensed at a program level
     # Store eval measure or additional outputs in
@@ -40,23 +72,22 @@ def compute_eval_measure(mdata, n_jobs=-1, prog_key='prog',
 
     # See https://anndata.readthedocs.io/en/latest/tutorials/notebooks/getting-started.html
 
+    # Return only the evaluations if not in inplace mode
     if not inplace: return mdata[prog_key].var['eval_measure']
 	
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
 
     parser.add_argument('mudataObj_path')
-    parser.add_argument('-n', '--n_jobs', default=1, type=int)
+    parser.add_argument('--somearg', default=None)
     parser.add_argument('-pk', '--prog_key', default='prog', type=str) 
-    parser.add_argument('-rk', '--rna_key', default='rna', type=str) 
-    parser.add_argument('-ak', '--atac_key', default='atac', type=str) 
+    parser.add_argument('-dk', '--data_key', default='rna', type=str) 
+    parser.add_argument('-n', '--n_jobs', default=1, type=int)
     parser.add_argument('--output', action='store_false') 
 
     args = parser.parse_args()
 
-    import mudata
     mdata = mudata.read(args.mudataObj_path)
-
-    compute_eval_measure(mdata, n_jobs=args.n_jobs,prog_key=args.prog_key, 
-                         rna_key=args.rna_key, atac_key=args.atac_key, inplace=args.output)
+    compute_eval_measure(mdata, prog_key=args.prog_key, data_key=args.data_key, 
+                         somearg=args.somearg, n_jobs=args.n_jobs, inplace=args.output)
 
